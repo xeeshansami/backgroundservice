9c13b6eb7de8a2d01580e9c67a9540c851b7a313
mac <info.xeeshan@gmail.com>
2023-01-20 22:17:46 +0500

    latest

---
+++ app/src/main/java/com/paxees/sms/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.paxees.sms
+
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    var mServiceIntent: Intent? = null
+    private var mYourService: YourService? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mYourService = YourService()
+        mServiceIntent = Intent(this, mYourService!!.javaClass)
+        if (!isMyServiceRunning(mYourService!!.javaClass)) {
+            startService(mServiceIntent)
+        }
+    }
+
+    private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                Log.i("Service status", "Running")
+                return true
+            }
+        }
+        Log.i("Service status", "Not running")
+        return false
+    }
+
+    override fun onDestroy() {
+        //stopService(mServiceIntent);
+        val broadcastIntent = Intent()
+        broadcastIntent.action = "restartservice"
+        broadcastIntent.setClass(this, Restarter::class.java)
+        this.sendBroadcast(broadcastIntent)
+        super.onDestroy()
+    }
+}
